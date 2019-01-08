cf31ea780e9f97e99be5330882e66d24a980e79d
maydx <madongxun@gmail.com>
2019-01-08 00:19:49 -0800

    Started proj1b

--- proj1b/LinkedListDeque.java
+++ proj1b/LinkedListDeque.java
@@ -1,4 +1,4 @@
-public class LinkedListDeque<T> {
+public class LinkedListDeque<T> implements Deque<T> {
     private class Node {
         public Node prev;
         public T item;
@@ -27,6 +27,7 @@ public class LinkedListDeque<T> {
     }
 
 
+    @Override
     public void addFirst(T item) {
         if (isEmpty()){
         sentinel.next = new Node(sentinel, item, sentinel);
@@ -38,6 +39,7 @@ public class LinkedListDeque<T> {
         size += 1;
     }
 
+    @Override
     public void addLast(T item){
         if (isEmpty()){
             sentinel.prev = new Node(sentinel, item, sentinel);
@@ -49,14 +51,17 @@ public class LinkedListDeque<T> {
         size += 1;
     }
 
+    @Override
     public boolean isEmpty(){
         return size == 0;
     }
 
+    @Override
     public int size(){
         return size;
     }
 
+    @Override
     public void printDeque() {
         Node p = sentinel.next;
         for(int i = 0; i < size(); i++){
@@ -66,6 +71,7 @@ public class LinkedListDeque<T> {
         System.out.println();
     }
 
+    @Override
     public T removeFirst(){
         if(sentinel.next == null){
             return null;
@@ -84,6 +90,7 @@ public class LinkedListDeque<T> {
         }
     }
 
+    @Override
     public T removeLast(){
         if(sentinel.prev.item == null){
             return null;
@@ -102,6 +109,7 @@ public class LinkedListDeque<T> {
         }
     }
 
+    @Override
     public T get(int index){
         Node p = sentinel.next;
         for (int i = 0; i < index + 1; i++ ){
